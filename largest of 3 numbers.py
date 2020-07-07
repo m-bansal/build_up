e4a18d4e8582504028464c9551633ddff8ea6491
m-bansal <67611924+m-bansal@users.noreply.github.com>
2020-07-07 13:19:02 -0300

    Day 7 of #100daysofcode

---
+++ largest of 3 numbers.py
@@ -0,0 +1,10 @@
+a,b,c=input(),input(),input()
+if a>b:
+    if a>c:
+        print(a,"is the biggest")
+    else:
+        print(c,"is the biggest")
+elif b>c:
+    print(b,"is the biggest")
+else:
+    print(c,"is the biggest")
