841a7d104e016a179e107a05e298fe7fed323722
m-bansal <67611924+m-bansal@users.noreply.github.com>
2020-08-01 13:43:06 -0300

    Day 32 of #100daysofcode

---
+++ Birthday cake candles.py
@@ -0,0 +1,8 @@
+def birthdayCakeCandles(ar):
+    return ar.count(max(ar))
+
+if __name__ == '__main__':    
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = birthdayCakeCandles(ar)
+    print(result)
