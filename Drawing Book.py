20d739b16360551a2e1376134c5475f201d2faed
m-bansal <67611924+m-bansal@users.noreply.github.com>
2020-08-16 10:34:43 -0300

    Day 46 of#100daysofcode

---
+++ Drawing Book.py
@@ -0,0 +1,26 @@
+def pageCount(n, p):
+    even=0
+    if(n%2==0):
+        even=1
+    
+    if(p==1 or p==n):
+        return('0')
+    elif((p-1) < (n-p)):
+        return(p//2)
+    else:
+        if (even==0):
+            return((n-p)//2)
+        else:
+            if((n-p)%2 == 0):
+                return((n-p)//2)
+            else:
+                return(((n-p)//2)+1)
+
+if __name__ == '__main__':
+    n = int(input())
+
+    p = int(input())
+
+    result = pageCount(n, p)
+
+    print(result)
