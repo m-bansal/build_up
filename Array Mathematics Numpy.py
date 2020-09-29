ffdae0fd63de39fd7e0071ac804bb9190805cf6f
m-bansal <67611924+m-bansal@users.noreply.github.com>
2020-09-28 22:23:15 -0400

    Day 89 of #100daysofcode

---
+++ Array Mathematics Numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+N, M = map(int, input().split())
+A =  np.array([list(map(int, input().split())) for i in range(N)])
+B =  np.array([list(map(int, input().split())) for i in range(N)])
+print (np.add(A, B))
+print (np.subtract(A, B))
+print (np.multiply(A, B))
+print (np.floor_divide(A, B))
+print (np.mod(A, B))
+print (np.power(A, B))
